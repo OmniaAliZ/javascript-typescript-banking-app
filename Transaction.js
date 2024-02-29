9d03b40cc8e59f1958b6a0913fe6fb4951cd1fad
sara-khaled1 <saraaljaber501@gmail.com>
2024-02-29 13:41:25 +0300

    Transaction

---
+++ Transaction.js
@@ -0,0 +1,10 @@
+class Transaction {
+  Constructor(amount, date) {
+    this.amount = amount
+    this.date = date
+
+  }
+
+}
+const Tran = new Transaction(200, "1-1-2024")
+console.log("Transaction" + Tran)
